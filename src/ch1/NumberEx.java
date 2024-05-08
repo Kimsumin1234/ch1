eb62431d862b4f8bae9c918ab46789c14f039587
sumin <whshaks14@gmail.com>
2024-05-08 12:11:09 +0900

    for문 추가

--- src/ch1/NumberEx.java
+++ src/ch1/NumberEx.java
@@ -2,6 +2,10 @@ package ch1;
 
 public class NumberEx {
     public static void main(String[] args) {
-
+        // 파일의 라이프 사이클 Modified 상태
+        // => 1차커밋 NumberEx 클래스 생성 후 for문 작성
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
