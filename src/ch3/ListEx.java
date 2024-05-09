e33acf97f401b944c6b44b9d5f2df04efaf68434
sumin <whshaks14@gmail.com>
2024-05-09 09:58:50 +0900

    List 객체 생성

--- src/ch3/ListEx.java
+++ src/ch3/ListEx.java
@@ -1,7 +1,10 @@
 package ch3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListEx {
     public static void main(String[] args) {
-
+        List<Integer> list = new ArrayList<>();
     }
 }
