e34d358833f59453bb697dca6c4745d69a44e117
sumin <whshaks14@gmail.com>
2024-05-09 10:05:28 +0900

    Revert "List 객체 생성"
    This reverts commit e33acf97f401b944c6b44b9d5f2df04efaf68434.

--- src/ch3/ListEx.java
+++ src/ch3/ListEx.java
@@ -1,15 +1,6 @@
 package ch3;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ListEx {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
-        list.add(1);
     }
 }
