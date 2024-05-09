fac076c1a520fbcc87fea9c138df71570b4d4c0c
sumin <whshaks14@gmail.com>
2024-05-09 09:59:22 +0900

    아이템 추가

--- src/ch3/ListEx.java
+++ src/ch3/ListEx.java
@@ -6,5 +6,10 @@ import java.util.List;
 public class ListEx {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(1);
     }
 }
